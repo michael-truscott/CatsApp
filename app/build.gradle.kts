e67e70e22f74d5d50cf67c1f25feaf9ffd197bac
Michael <michael.r.truscott@gmail.com>
2023-10-30 22:50:31 +1100

    Add Retrofit and Coil dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
