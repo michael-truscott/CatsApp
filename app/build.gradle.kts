f00efa672b3ba26fa32829238fa6df996e8bec90
Michael <michael.r.truscott@gmail.com>
2023-10-30 22:42:45 +1100

    Add Compose navigation & viewmodel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
