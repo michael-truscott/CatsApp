bb7c73ef1a80bc6813f6962d850cf47cfdfbf080
Michael <michael.r.truscott@gmail.com>
2023-11-06 20:01:42 +1100

    Home screen - Add basic layout for CatList

--- app/src/main/java/com/truscorp/catsapp/ui/common/CatUi.kt
+++ app/src/main/java/com/truscorp/catsapp/ui/common/CatUi.kt
@@ -2,5 +2,6 @@ package com.truscorp.catsapp.ui.common
 
 data class CatUi(
     val id: String,
-    val tags: List<String>
+    val tags: List<String> = listOf(),
+    val isFavourite: Boolean = false,
 )
